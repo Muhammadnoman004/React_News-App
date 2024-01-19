efc2e35a119a93ba394c3e54ba1010406c79e16d
Noman Shamim <nomanshamim2004@gmail.com>
2024-01-19 23:47:26 +0500

    Still Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(function () {
+    getDataFromApi()
+  }, [])
+
+  function getDataFromApi() {
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-12-19&sortBy=publishedAt&apiKey=a502c4892df7456e908c32e0a8ac631b')
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data.articles[0]);
+        data.articles.map((news) => {
+          console.log(news.title);
+        })
+
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="card">
+        <img src="" alt="" />
+        <div className="card-body">
+          <h5 className="card-title">Tittle</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
     </div>
   );
 }
